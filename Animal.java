8283af70221acf25b27ade14818c3e00ea6845d5
Andrey <Andre.68.1@mail.ru>
2023-07-08 17:00:47 +0500

    creating an instance

---
+++ Animal.java
@@ -0,0 +1,11 @@
+
+public class Animal {
+
+    private String name;
+    private String animal;
+
+    public Animal(String name, String animal) {
+        this.name=name;
+        this.animal=animal;
+    }
+}
